3e007452556138316c166b3261c176945511da9a
ghoffar8804@gmail.com <ghoffar8804@gmail.com>
2023-09-28 16:24:22 +0700

    include grade (percobaan 2)

--- Daspro/smt 1/jobsheet/week5/jobsheet/src/PemilihanPercobaan212.java
+++ Daspro/smt 1/jobsheet/week5/jobsheet/src/PemilihanPercobaan212.java
@@ -15,8 +15,20 @@ public class PemilihanPercobaan212 {
 
         float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
         
-        String message = total < 65 ? ("Remidi") : ("Tidak Remidi");
-
-        System.out.println("Nilai akhir: " + total + " sehingga " + message);
+        if (total > 80 && total <= 100) {
+            System.out.println("Nilai Huruf: A, Nilai Setara: 4, Kualifikasi: Sangat Baik");
+        } else if (total > 73 && total <= 80) {
+            System.out.println("Nilai Huruf: B+, Nilai Setara: 3.5, Kualifikasi: Lebih dari Baik");
+        } else if (total > 65 && total <= 73) {
+            System.out.println("Nilai Huruf: B, Nilai Setara: 3, Kualifikasi: Baik");
+        } else if (total > 60 && total <= 65) {
+            System.out.println("Nilai Huruf: C+, Nilai Setara: 2.5, Kualifikasi: Lebih dari Cukup");
+        } else if (total > 50 && total <= 60) {
+            System.out.println("Nilai Huruf: C, Nilai Setara: 2, Kualifikasi: Cukup");
+        } else if (total > 39 && total <= 50) {
+            System.out.println("Nilai Huruf: D, Nilai Setara: 1, Kualifikasi: Kurang");
+        } else if (total <= 39){
+            System.out.println("Nilai Huruf: E, Nilai Setara: 0, Kualifikasi: Gagal");        
+        }
     }
 }
