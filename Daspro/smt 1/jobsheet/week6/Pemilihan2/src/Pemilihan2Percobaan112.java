f1f34de3c2895b6682228405547b90275029eafa
ghoffar8804@gmail.com <ghoffar8804@gmail.com>
2023-10-03 23:30:22 +0700

    soal4 (percobaan 1)

--- Daspro/smt 1/jobsheet/week6/Pemilihan2/src/Pemilihan2Percobaan112.java
+++ Daspro/smt 1/jobsheet/week6/Pemilihan2/src/Pemilihan2Percobaan112.java
@@ -12,7 +12,7 @@ public class Pemilihan2Percobaan112 {
             if ((tahun % 100) != 0) {
                 System.out.println("Tahun Kabisat");
             } else if ((tahun % 400) != 0) {
-                
+                System.out.println("Tahun Kabisat");
             } else {
                 System.out.println("Bukan Tahun Kabisat");
             }
