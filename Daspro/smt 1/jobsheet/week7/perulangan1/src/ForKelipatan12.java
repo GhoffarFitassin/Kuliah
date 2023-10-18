18f99ac00c33e9dadcacdf789878e45333a2e4f6
ghoffar8804@gmail.com <ghoffar8804@gmail.com>
2023-10-18 12:29:10 +0700

    pertanyaan 3

--- Daspro/smt 1/jobsheet/week7/perulangan1/src/ForKelipatan12.java
+++ Daspro/smt 1/jobsheet/week7/perulangan1/src/ForKelipatan12.java
@@ -4,18 +4,22 @@ public class ForKelipatan12 {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
 
-        int kelipatan, total = 0, counter = 0;
+        int kelipatan, jumlah = 0, counter = 0;
+        float rataKelipatan = 0f;
 
         System.out.print("Masukkan bilangan kelipatan (1-9): ");
         kelipatan = scan.nextInt();
         
-        for (int i = 0; i < 50; i++) {
+        for (int i = 1; i <= 50; i++) {
             if (i % kelipatan == 0) {
-                total += i;
+                jumlah += i;
                 counter++;
+                rataKelipatan++;
             }
         }
+        rataKelipatan = jumlah / rataKelipatan;
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
-        System.out.printf("total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %f\n", kelipatan, rataKelipatan);
     }
 }
