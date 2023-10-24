99e726d85d87b935bf4a826738a0ab3423414941
ghoffar8804@gmail.com <ghoffar8804@gmail.com>
2023-10-24 11:01:31 +0700

    pertanyaan 4 (percobaan 2)

--- Matkul_Tahun_1/Daspro/smt 1/jobsheet/week9/array1/src/ArrayNilai12.java
+++ Matkul_Tahun_1/Daspro/smt 1/jobsheet/week9/array1/src/ArrayNilai12.java
@@ -14,6 +14,8 @@ public class ArrayNilai12 {
         for (int i = 0; i < nilaiAkhir.length; i++) {
             if (nilaiAkhir[i] > 70) {
                 System.out.println("Mahasiswa ke-"+i+" lulus!");
+            } else if (nilaiAkhir[i] <= 70){
+                System.out.println("Mahasiswa ke-"+i+" tidak lulus!");
             }
         }
     }
