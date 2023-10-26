107991c00a6f5b8811b84141ba6b2843a79bc196
ghoffar8804@gmail.com <ghoffar8804@gmail.com>
2023-10-26 11:51:33 +0700

    pertanyaan 2 (percobaan 3)

--- Matkul_Tahun_1/Daspro/smt 1/jobsheet/week9/array1/src/ArrayRataNilai12.java
+++ Matkul_Tahun_1/Daspro/smt 1/jobsheet/week9/array1/src/ArrayRataNilai12.java
@@ -3,28 +3,59 @@ import java.util.Scanner;
 public class ArrayRataNilai12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
 
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
+        int[] nilaiMhs = new int[5];
+        int jumlahMhs;
+        double total = 0, total2 = 0;
+        double rata2, rata;
+
+        System.out.print("Masukkan jumlah Mahasiswa: ");
+        jumlahMhs = sc.nextInt();
+        for (int i = 0; i < jumlahMhs; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
         }
+
+        int lulusCount = 0;
+        int tidakLulusCount = 0;
         
-        for (int i = 0; i < nilaiMhs.length; i++) {
+        for (int i = 0; i < jumlahMhs; i++) {
             if (nilaiMhs[i] > 70) {
-                System.out.println("Mahasiswa ke-"+i+" lulus!");
-            } 
+                total += nilaiMhs[i];
+                lulusCount++;
+            } else {
+                total2 += nilaiMhs[i];
+                tidakLulusCount++;
+            }
+        }
+        
+        if (lulusCount > 0) {
+            rata = total / lulusCount;
+            System.out.println("Rata-rata nilai lulus = " + rata);
         }
 
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+        if (tidakLulusCount > 0) {
+            rata2 = total2 / tidakLulusCount;
+            System.out.println("Rata-rata nilai tidak lulus = " + rata2);
         }
 
+        // for (int i = 0; i < nilaiMhs.length; i++) {
+        //     if (nilaiMhs[i] > 70) {
+        //         total += nilaiMhs[i];
+        //     } else if (nilaiMhs[i] <= 70){
+        //         total2 += nilaiMhs[i];
+        //     }
+        // }
 
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
+        // rata = total / nilaiMhs.length; 
+        // rata2 = total2 / nilaiMhs.length;
+        // System.out.println("Rata-rata nilai lulus = " + rata);
+        // System.out.println("Rata-rata nilai tidak lulus = " + rata2);
     }
 }
+
+        // for (int i = 0; i < nilaiMhs.length; i++) {
+        // if (nilaiMhs[i] > 70) {
+        // System.out.println("Mahasiswa ke-"+i+" lulus!");
+        // }
+        // }
