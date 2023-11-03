f10d837e0fadcc27fdd5d30034d5b8353be54871
ghoffar8804@gmail.com <ghoffar8804@gmail.com>
2023-11-03 07:47:04 +0700

    feat : Pertanyaan 2 (Percobaan 2)

--- Matkul_Tahun_1/Daspro/smt 1/jobsheet/week10/array-2/src/BioskopWithScanner12.java
+++ Matkul_Tahun_1/Daspro/smt 1/jobsheet/week10/array-2/src/BioskopWithScanner12.java
@@ -5,24 +5,60 @@ public class BioskopWithScanner12 {
         Scanner sc = new Scanner(System.in);
 
         String nama, next;
-        int baris, kolom;
+        int baris, kolom, pilih;
+        boolean isMenu = true;
+        boolean notMenu = false;
         String[][] penonton = new String[4][2];
-    
-        while (true) {
-            System.out.print("Masukkan nama: ");
-            nama = sc.next();
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
 
-            penonton[baris-1][kolom-1] = nama;
-
-            System.out.print("Inputan penonton lainnya? (y/n): ");
-            next = sc.next();
-
-            if (next.equals("n")) {
-                break;
+        while (isMenu != false) {
+            System.out.println("Menu 1: Input Data Penonton");
+            System.out.println("Menu 2: Tampil Daftar Penonton");
+            System.out.println("Menu 3: Exit");
+            System.out.print("Pilih menu:  ");
+            pilih = sc.nextInt();
+            switch (pilih) {
+                case 1:
+                    isMenu = false;
+                    do {
+                        System.out.print("Masukkan nama: ");
+                        nama = sc.next();
+                        System.out.print("Masukkan baris: ");
+                        baris = sc.nextInt();
+                        System.out.print("Masukkan kolom: ");
+                        kolom = sc.nextInt();
+                        penonton[baris - 1][kolom - 1] = nama;
+                        System.out.print("Inputan penonton lainnya? (y/n): ");
+                        next = sc.next();
+                        if (next.equals("n")) {
+                            notMenu = false;
+                            isMenu = true;
+                        } else {
+                            isMenu = false;
+                            notMenu = true;
+                        }
+                    } while (notMenu != false);
+                    break;
+                case 2:
+                    isMenu = false;
+                    do {
+                        System.out.println("==================================================");
+                        System.out.println("\t\tDaftar Penonton");
+                        for (int i = 0; i < penonton.length; i++) {
+                            System.out.println(
+                                    "Penonton pada baris ke-" + (i + 1) + ": " + String.join(", ", penonton[i]));
+                        }
+                        System.out.println("==================================================");
+                        System.out.print("Kembali ke Menu Utama?? (y/n): ");
+                        next = sc.next();
+                        if (next.equals("y")) {
+                            notMenu = false;
+                            isMenu = true;
+                        }
+                    } while (notMenu != false);
+                    break;
+                case 3:
+                    isMenu = false;
+                    System.out.println("Terima Kasih (^_^)");
             }
         }
     }
