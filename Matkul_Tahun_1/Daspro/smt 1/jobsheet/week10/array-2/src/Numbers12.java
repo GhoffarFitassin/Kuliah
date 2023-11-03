fbc54d87931c72c9a672d9a23887ffee5d4d9c4c
ghoffar8804@gmail.com <ghoffar8804@gmail.com>
2023-11-03 08:39:57 +0700

    feat : pertanyaan 4 (Percobaan 3)

--- Matkul_Tahun_1/Daspro/smt 1/jobsheet/week10/array-2/src/Numbers12.java
+++ Matkul_Tahun_1/Daspro/smt 1/jobsheet/week10/array-2/src/Numbers12.java
@@ -7,8 +7,11 @@ public class Numbers12 {
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
         
+        // for (int i = 0; i < myNumbers.length; i++) {
+        //     System.out.println(Arrays.toString(myNumbers[i]));
+        // }
         for (int i = 0; i < myNumbers.length; i++) {
-            System.out.println(Arrays.toString(myNumbers[i]));
+            System.out.println("Panjang pada baris ke-" + (i+1) + ": " + myNumbers[i].length);
         }
     }
 }
