1cdf11b0aaef1f1bbf770a37ab5b2a83c1b1c33a
ghoffar8804@gmail.com <ghoffar8804@gmail.com>
2023-11-22 21:59:17 +0700

    feat : delete throws exception

--- Matkul_Tahun_1/Daspro/smt 1/jobsheet/week13/fungsi1/src/TerimaKasih_12.java
+++ Matkul_Tahun_1/Daspro/smt 1/jobsheet/week13/fungsi1/src/TerimaKasih_12.java
@@ -4,7 +4,7 @@ public class TerimaKasih_12 {
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         ucapanTerimakasih();
     }
 }
