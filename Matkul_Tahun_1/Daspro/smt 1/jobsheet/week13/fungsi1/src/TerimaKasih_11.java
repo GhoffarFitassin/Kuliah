b7f809bfb1f70204aab4bb37ae66dcae52c75762
ghoffar8804@gmail.com <ghoffar8804@gmail.com>
2023-11-22 22:55:35 +0700

    Refactor: change no absen

--- Matkul_Tahun_1/Daspro/smt 1/jobsheet/week13/fungsi1/src/TerimaKasih_11.java
+++ Matkul_Tahun_1/Daspro/smt 1/jobsheet/week13/fungsi1/src/TerimaKasih_11.java
@@ -1,4 +1,4 @@
-public class TerimaKasih_12 {
+public class TerimaKasih_11 {
     public static void ucapanTerimakasih(){
         System.out.println("Thank you for being the best teacher in the world.\n" + 
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
