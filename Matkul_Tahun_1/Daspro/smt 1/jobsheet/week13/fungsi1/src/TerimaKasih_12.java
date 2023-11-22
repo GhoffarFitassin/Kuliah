f15db58dcffe2d272debe663417cd25de67bde11
ghoffar8804@gmail.com <ghoffar8804@gmail.com>
2023-11-22 22:11:48 +0700

    feat: pertanyaan 2 (percobaan 1)

--- Matkul_Tahun_1/Daspro/smt 1/jobsheet/week13/fungsi1/src/TerimaKasih_12.java
+++ Matkul_Tahun_1/Daspro/smt 1/jobsheet/week13/fungsi1/src/TerimaKasih_12.java
@@ -1,10 +1,10 @@
 public class TerimaKasih_12 {
     public static void ucapanTerimakasih(){
-        System.out.println("Thank you for being the best teacher in the world.\n" + 
-        "You inspired in me a love for learning and made me feel like I could ask you anything.");
+        System.out.println("Thank you for being the best teacher in the world");
     }
 
     public static void main(String[] args){
         ucapanTerimakasih();
+        System.out.println("You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
 }
