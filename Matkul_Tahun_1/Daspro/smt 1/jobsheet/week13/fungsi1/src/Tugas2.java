2dfa8c9225176a52dd4b033426e69de3e0333ec2
ghoffar8804@gmail.com <ghoffar8804@gmail.com>
2023-11-24 10:36:49 +0700

    refactor: pertanyaan 3 (tugas)

--- Matkul_Tahun_1/Daspro/smt 1/jobsheet/week13/fungsi1/src/Tugas2.java
+++ Matkul_Tahun_1/Daspro/smt 1/jobsheet/week13/fungsi1/src/Tugas2.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
 public class Tugas2 {
-    static int[][] arrayNilai = new int[5][7];
+    static Scanner sc = new Scanner(System.in);
+    static int jumlahMahasiswa = sc.nextInt();
+    static int jumlahtugas = sc.nextInt();
+    static int[][] arrayNilai = new int[jumlahMahasiswa][jumlahtugas];
 
     static void inputData(){
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < arrayNilai[0].length; i++) {
-            System.out.println("Minggu ke : " + (i+1));
+            System.out.println("Tugas ke : " + (i+1));
             for (int j = 0; j < arrayNilai.length; j++) {
                 System.out.println("Masukkan Nilai Mahasiswa ke-"+j+": ");
                 arrayNilai[j][i] = sc.nextInt();
@@ -16,7 +19,7 @@ public class Tugas2 {
 
     static void outputData(){
         for (int i = 0; i < arrayNilai[0].length; i++) {
-            System.out.println("Minggu ke : " + (i+1));
+            System.out.println("Tugas ke : " + (i+1));
             for (int j = 0; j < arrayNilai.length; j++) {
                 System.out.println("Nilai Mahasiswa ke-"+j+": "+ arrayNilai[j][i] );
             }
@@ -24,30 +27,30 @@ public class Tugas2 {
     }
 
     static void highestWeek(){
-        int nilai = 0 , minggu = -1;
+        int nilai = 0 , tugas = -1;
         for (int i = 0; i < arrayNilai[0].length; i++) {
             for (int j = 0; j < arrayNilai.length; j++) {  
                 if (nilai < arrayNilai[j][i]) {
                     nilai = arrayNilai[j][i];
-                    minggu = i;
+                    tugas = i;
                 }
             }
         }
-        System.out.println("Nilai tertinggi ada pada minggu ke-"+(minggu+1));
+        System.out.println("Nilai tertinggi ada pada Tugas ke-"+(tugas+1));
     }
 
     static void highestScore(){
-        int nilai = 0 , minggu = -1, mahasiswa = -1;
+        int nilai = 0 , tugas = -1, mahasiswa = -1;
         for (int i = 0; i < arrayNilai[0].length; i++) {
             for (int j = 0; j < arrayNilai.length; j++) {  
                 if (nilai < arrayNilai[j][i]) {
                     nilai = arrayNilai[j][i];
-                    minggu = i;
+                    tugas = i;
                     mahasiswa = j;
                 }
             }
         }
-        System.out.println("Nilai tertinggi diraih oleh mahasiswa ke-"+mahasiswa+" pada minggu ke-"+(minggu+1)+" dengan nilai "+nilai);
+        System.out.println("Nilai tertinggi diraih oleh mahasiswa ke-"+mahasiswa+" pada Tugas ke-"+(tugas+1)+" dengan nilai "+nilai);
     }
 
     public static void main(String[] args) {
