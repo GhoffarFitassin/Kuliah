1e42a8181e3a5c62487fd18220f16fc3183e6f5c
GhoffarFitassin <ghoffar8804@gmail.com>
2026-02-20 14:52:29 +0700

    update: jobsheet week 2

---
+++ Matkul_Semester_6/Pemrograman Berbasis Framework/week 2/praktikum/my-app/src/pages/about.tsx
@@ -0,0 +1,24 @@
+export default function About() {
+  return (
+    <div>
+      <h1>Tentang Saya</h1>
+      <table>
+          <tr>
+            <td>Nama Mahasiswa</td>
+            <td>:</td>
+            <td>Ghoffar Abdul Ja'far</td>
+          </tr>
+          <tr>
+            <td>NIM</td>
+            <td>:</td>
+            <td>2341720035</td>
+          </tr>
+          <tr>
+            <td>Program Studi</td>
+            <td>:</td>
+            <td>D4 Teknik Informatika</td>
+          </tr>
+      </table>
+    </div>
+  );
+}
