032c9dd34abb51d700ca073c882d0e14e9b37b56
GhoffarFitassin <ghoffar8804@gmail.com>
2026-06-20 14:41:55 +0700

    JS 15 week 8

---
+++ Matkul_Semester_6/Pemrograman Berbasis Framework/praktikum/my-app/src/pages/auth/register.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import TampilanRegister from "@/views/auth/register/Index";
+
+const halamanRegister = () => {
+  return (
+    <TampilanRegister />
+  );
+};
+
+export default halamanRegister;
